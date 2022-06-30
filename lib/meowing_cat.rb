6001f94096f31746ace19d292b6a9602365af4d8
--replace-all <terrencejung928@gmail.com>
2022-06-30 10:48:08 -0700

    commit tree 2a4fcf04e4f7b5d9c48e26298aa259e0f8e821af

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+class Cat
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+end
+
+kiyo = Cat.new
+kiyo.meow
+kiyo.name = "kiyo"
+puts kiyo.name
